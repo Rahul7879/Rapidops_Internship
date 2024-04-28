5fd05efaa101fc229b65ba3bab1810f870b88963
Rahul <rahul.rajput@rapidops.co>
2024-04-28 15:30:04 +0530

    Node Task-03 of Exercise-01 modified

--- NodeJS/Exercise-01/Task-03/index.js
+++ NodeJS/Exercise-01/Task-03/index.js
@@ -1,74 +1,63 @@
-const fs = require("fs");
-const pfs = require("fs").promises;
+const fs = require("fs").promises;
 const path = require('path');
+const zlib = require('zlib');
+const { createReadStream, createWriteStream } = require('fs');
 
-async function writeHeader() {
-        fs.writeFile('./folderSize.csv', 'Directory,Size \n',(err)=>{
-            if(err){
-                console.log("file not created")
-            }
-        });
-}
-
-async function calculateFolderSize1(dirName) {
-    let files = await pfs.readdir(dirName);
-    let totalSize = 0;
-
-    for (let file of files) {
-        let filePath = path.join(dirName, file);
-        let stats = await pfs.stat(filePath);
-
-        if (stats.isDirectory()) {
-            let dirSize = await calculateFolderSize1(filePath);
-            await pfs.appendFile('./folderSize.csv', `${file},${dirSize}\n`);
-            totalSize += dirSize;
-        } else {
-            totalSize += stats.size;
-        }
+const writeCsvHeader = async () => {
+    try {
+        await fs.writeFile('./folderSize.csv', 'Directory,Size \n');
+    } catch (error) {
+        console.error("Failed to create file:", error);
     }
-    return totalSize;
-}
+};
 
-function calculateFolderSize2(dirName) {
-    let allDirectory = fs.readdirSync(dirName);
-    let folderSize = 0;
-    for (let i of allDirectory) {
-        let a = fs.statSync(path.join(dirName, i));
-        let flag = !path.extname(i);
-        let untrackedFiles = ["license",".npmignore","makefile",".eslintrc","mime"]
-        if (untrackedFiles.includes(i.toLowerCase())) {
-            folderSize += a.size;
-        }
-        else if (flag) {
-            let size = calculateFolderSize2(path.join(dirName, i));
-            console.log("file", i, "size", size);
-            fs.appendFileSync("./folderSize.csv", i + "," + size + "\n");
-            folderSize += (size);
+const calculateDirectorySize = async (directoryPath) => {
+    const fileList = await fs.readdir(directoryPath);
+    const sizeTasks = fileList.map(async (file) => {
+        const filePath = path.join(directoryPath, file);
+        const fileStats = await fs.stat(filePath);
+        
+        if (fileStats.isDirectory()) {
+            const directorySize = await calculateDirectorySize(filePath);
+            await fs.appendFile('./folderSize.csv', `${file},${directorySize}\n`);
+            return directorySize;
         } else {
-            folderSize += a.size;
+            return fileStats.size;
         }
-
-    }
-    return folderSize;
-}
-
-function compressCSV(){
-    const zlib = require('zlib');
-    const readStream = fs.createReadStream('folderSize.csv');
-    const writeStream = fs.createWriteStream('compFolderSize.gzip');
-    const gzip = zlib.createGzip();
-    readStream.pipe(gzip).pipe(writeStream).on('finish', () => {
-        console.log('File successfully compressed');
     });
-}
 
-async function readFolder(dirName) {
-    await writeHeader();
-    let size = await calculateFolderSize1(path.join(__dirname,dirName));
-    // let size =  calculateFolderSize2(path.join(__dirname,dirName));
-    console.log("Total size of directory", dirName, "is", size);
-    compressCSV()
+    const fileSizes = await Promise.all(sizeTasks);
+    return fileSizes.reduce((totalSize, fileSize) => totalSize + fileSize, 0);
+};
 
-}
+const compressCsvFile = async () => {
+    const csvFilePath = 'folderSize.csv';
+    const compressedFilePath = 'compFolderSize.gzip';
+    const readStream = createReadStream(csvFilePath);
+    const writeStream = createWriteStream(compressedFilePath);
+    const gzip = zlib.createGzip();
 
-readFolder("node_modules");
+    return new Promise((resolve, reject) => {
+        readStream.pipe(gzip).pipe(writeStream).on('finish', () => {
+            console.log('CSV file successfully compressed');
+            resolve();
+        }).on('error', reject);
+    });
+};
+
+const analyzeDirectorySize = async (directoryName) => {
+    try {
+        await writeCsvHeader();
+        const totalSize = await calculateDirectorySize(path.join(__dirname, directoryName));
+        console.log("Total size of directory", directoryName, "is", totalSize);
+        await compressCsvFile();
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            console.log("folder does not exist!");
+        } else {
+            console.error("Error processing directory:", error);
+        }
+    }
+};
+
+analyzeDirectorySize("node_modules");
