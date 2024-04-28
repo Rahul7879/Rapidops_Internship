24f489755ab967dbe3b411e54bc541a612cf8aec
Rahul <rahul.rajput@rapidops.co>
2024-04-28 14:40:59 +0530

    Node Task-02 of Exercise-01 is modified

--- NodeJS/Exercise-01/Task-02/index.js
+++ NodeJS/Exercise-01/Task-02/index.js
@@ -1,96 +1,113 @@
-const fs = require("fs");
-const crypto = require("crypto");
+const fs = require('fs').promises;
+const crypto = require('crypto');
 const readline = require('readline');
+const path = require('path');
 const { stdin: input, stdout: output } = require('process');
+const databaseFilePath = path.join(__dirname, 'dataBase.json'); 
 const rl = readline.createInterface({ input, output });
-
 
 function generateSalt(length = 16) {
     return crypto.randomBytes(length).toString('hex');
 }
 
 function hashPassword(password) {
-    let salt = generateSalt()
+    const salt = generateSalt();
     const hash = crypto.createHmac('sha256', salt);
     hash.update(password);
-    const value = hash.digest('hex');
     return {
-        salt: salt,
-        hashedPassword: value
+        salt,
+        hashedPassword: hash.digest('hex')
     };
 }
 
 function verifyPassword(storedHash, storedSalt, passwordToVerify) {
     const hash = crypto.createHmac('sha256', storedSalt);
     hash.update(passwordToVerify);
-    const value = hash.digest('hex');
-    return value === storedHash;
+    return hash.digest('hex') === storedHash;
 }
 
-let userSignup = (username, password) => {
-    fs.readFile("./dataBase.json", "utf-8", (err, data) => {
-        let dataBase = {}
-        if (err) {
-            dataBase[username] = { password: hashPassword(password) }
-            fs.writeFile('./dataBase.json', JSON.stringify(dataBase), () => {
-                console.log("new db file created");
-            })
-        } else {
-            dataBase = JSON.parse(data);
-            if (dataBase[username] !== undefined) {
-                console.log("user already exists")
-                return;
-            } else {
-                dataBase[username] = { password: hashPassword(password) }
-                fs.writeFile('./dataBase.json', JSON.stringify(dataBase), () => {
-                    console.log("Congratulations! Sign up complete")
-                })
-            }
+async function signupUser(username, password) {
+    try {
+        const dataBase = await fs.readFile(databaseFilePath, "utf-8");
+        const userDatabase = JSON.parse(dataBase);
+        if (userDatabase[username]) {
+            console.log("User already exists");
+            return;
         }
-    })
-}
-
-
-let userLogin = (username, password) => {
-    fs.readFile('./dataBase.json', "utf-8", (err, data) => {
-        let dataBase = JSON.parse(data);
-        if (dataBase[username] === undefined) {
-            console.log("User Not Exists!");
-            return
+        userDatabase[username] = { password: hashPassword(password) };
+        await fs.writeFile(databaseFilePath, JSON.stringify(userDatabase));
+        console.log("Congratulations! Sign up complete");
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            const userDatabase = { [username]: { password: hashPassword(password) } };
+            await fs.writeFile(databaseFilePath, JSON.stringify(userDatabase));
+            console.log("Congratulations! Sign up complete");
         } else {
-            let hashPassword = dataBase[username].password.hashedPassword;
-            let salt = dataBase[username].password.salt;
-            if (verifyPassword(hashPassword, salt, password)) {
-                console.log("Welcome", username);
-            } else {
-                console.log("Wrong password")
-            }
+            throw err;
         }
-    })
+    }
 }
 
+async function loginUser(username, password) {
+    try {
+        const dataBase = await fs.readFile(databaseFilePath, "utf-8");
+        const userDatabase = JSON.parse(dataBase);
+        if (!userDatabase[username]) {
+            console.log("User does not exist!");
+            return;
+        }
+        const { hashedPassword, salt } = userDatabase[username].password;
+        if (verifyPassword(hashedPassword, salt, password)) {
+            console.log("Welcome", username);
+        } else {
+            console.log("Wrong password");
+        }
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            console.log("user does not exist!");
+        } else {
+            throw err;
+        }
+    }
+}
 
-rl.question('Enter action: ', (action) => {
-    rl.question('Enter name: ', (name) => {
-
-        rl.stdoutMute = true;
-        output.write('Enter password: ');
-        rl.on('line', (password) => {
-            rl.stdoutMute = false;
-
-            if (action === "login") {
-                userLogin(name, password);
-            } else if (action === "signup") {
-                userSignup(name, password);
-            }
-            console.log("\n")
-            rl.close();
+async function prompt(questionText) {
+    return new Promise((resolve) => {
+        rl.question(questionText, (answer) => {
+            resolve(answer);
         });
-        rl._writeToOutput = function _writeToOutput(stringToWrite) {
-            if (rl.stdoutMute)
-                output.write("*");
-            else
-                output.write(stringToWrite);
-        };
     });
-});
+}
+
+async function getUserInput() {
+    try {
+        const action = await prompt('Enter action (login or signup): ');
+        if (action !== "login" && action !== "signup") {
+            console.log("\nInvalid Action");
+            return;
+        }
+
+        const username = await prompt('Enter username: ');
+        if (!username.trim()) {
+            throw new Error("Username cannot be empty.");
+        }
+
+        const password = await prompt('Enter Password: ');
+
+        if (!password.trim()) {
+            throw new Error("Password cannot be empty.");
+        }
+
+        if (action === "login") {
+            await loginUser(username, password);
+        } else if (action === "signup") {
+            await signupUser(username, password);
+        }
+    } catch (err) {
+        console.log(err.message);
+    } finally {
+        rl.close();
+    }
+}
+
+getUserInput();
